84ec0b5899dc2d40114d7c9ea5f079e39b2af02a
Mats Fredriksson <matsf@google.com>
2018-11-12 20:16:12 +0100

    cleanup

--- src/gtkmain/src/main.cpp
+++ src/gtkmain/src/main.cpp
@@ -5,16 +5,6 @@
 
 int main(int argc, char* argv[])
 {
-  /*
-  auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example", Gio::APPLICATION_HANDLES_OPEN);
-
-  const char* filename = "/home/mats/code/hello-world-gtk/CMakeLists.txt";
-  MyWindow window(filename);
-  window.set_title("Drawing text example");
-
-  window.set_modal(true);
-  return app->run(window);
-   */
   auto app = MyApplication::create();
   return app->run(argc, argv);
 }
